74dba32a725ef62b1049cb36f2810b11a5f4d5c1
ToshiZ <andi4andi@gmail.com>
2014-11-16 03:15:52 +0300

    16.11
    change for to timeOuts

--- script_in_content.js
+++ script_in_content.js
@@ -4,10 +4,8 @@
 		ticketsJson,
 		divsRel = [],
 		tbodyTeams = [],
-		teamsJson = {"team":[]},
-		forClick = [];
+		teamsJson = {"team":[]};
 		
-										
 		
 	var tbb = $('tbody[data-event-name]');
 	tbb.each(function(i){
@@ -22,92 +20,104 @@
 			tbodyTeams.push($(this));
 		}
 	});	
+	
 	if(teamsJson.team[0]){
 		localStorage.removeItem('teams');
 		localStorage.setItem('teams', JSON.stringify(teamsJson));
 		chrome.runtime.sendMessage({askFor: 'teamsFromMB', teams: JSON.stringify(teamsJson)});
 		chrome.runtime.onMessage.addListener(
-			 function(request, sender, sendResponse) {		
+			 function(request, sender, sendResponse) {				
 				if (request.askFor == "tickets"){
+					var start = new Date().getTime();
 					ticketsJson = JSON.parse(request.tickets);
 					localStorage.setItem('tickets', JSON.stringify(ticketsJson));
-					tbodyTeams.forEach(function(item, index){
-						for(var j = 0; j < ticketsJson.ticket[0].length; j++){
-							if(item.attr('data-event-name').indexOf(ticketsJson.ticket[0][j].name) != -1){
-								var tdDate = item.find('td.date').html();
-								if(tdDate)
-									if(tdDate.indexOf(ticketsJson.ticket[0][j].date) != -1){
-										if(ticketsJson.ticket[0][j].bet == 1){
-											forClick.push(item.find('tr:first-child').find('td').eq(-1));
-											break;
+						var i = 0;
+						setBets();
+						function setBets(){
+							var bigCycle = setTimeout(function(){
+								//var winMessage = ('p#betresult').html();
+								//if($('#ok-button.no.simplemodal-close').length){
+									if($('p#betresult').html().indexOf("$betresult") == -1){
+										if($('p#betresult').html().indexOf("Ваша ставка принята, спасибо") != -1){	
+											if($('#ok-button.no.simplemodal-close')[0]);
+												$('#ok-button.no.simplemodal-close')[0].click();
+											betTicket();
+										}else{
+											clearTimeout(bigCycle);
+											alert($('p#betresult').html());
+										}										
+									}else{
+										if(i != 0){
+											betTicket();
+											/* clearTimeout(bigCycle);
+											var inter = setTimeout(function(){
+												setBets();
+											}, 500); */
+										}else
+											betTicket();
+									}
+								}, 1000);
+							}
+							
+							function betTicket(){
+								var forClick = [];
+								tbodyTeams.forEach(function(item, index){
+										for(var j = 0; j < ticketsJson.ticket[i].length; j++){       		
+											if(item.attr('data-event-name').indexOf(ticketsJson.ticket[i][j].name) != -1){
+												var tdDate = item.find('td.date').html();
+												if(tdDate)
+													if(tdDate.indexOf(ticketsJson.ticket[i][j].date) != -1){
+														if(ticketsJson.ticket[i][j].bet == 1){
+															forClick.push(item.find('tr:first-child').find('td').eq(-1));
+															break;
+														}
+														if(ticketsJson.ticket[i][j].bet == 0){
+															forClick.push(item.find('tr:first-child').find('td').eq(-2));
+															break;
+														}
+													}
+											}
 										}
-										if(ticketsJson.ticket[0][j].bet == 0){
-											forClick.push(item.find('tr:first-child').find('td').eq(-2));
-											break;
-										}
+									});
+									var it = 0;
+									markTeams();
+									function markTeams(){
+										var self = forClick[it];
+										var markTeamsTimer = setTimeout(function(){
+												$(self).click();
+												if(it < forClick.length){
+													it++;
+													markTeams();
+												}
+												else{
+													setCoast(6);
+													clearTimeout(markTeamsTimer);
+												}
+											},1000);
+									}
+									function setCoast(coast){					
+										$('#button_accumulator')[0].click();
+										var timer1 = setTimeout(function(){										
+												var evt = document.createEvent('KeyboardEvent');
+												evt.initKeyboardEvent('keyup', true, true, window, false, false, false, false, 13, 13);
+												evt.keyCode = 13;
+												evt.which = 13;
+												evt.charCode = 13; 
+												$('.stake.stake-input.js-focusable[name = stake]').val(coast);
+												$('.stake.stake-input.js-focusable[name = stake]')[0].dispatchEvent(evt);
+											}, 1000);
+										var timer2 = setTimeout(function(){
+												//$('.but-place-bet')[0].click();
+												$('a.but-remove')[0].click();
+												if(i < ticketsJson.ticket.length-1){
+													i++;
+													setBets();
+												}
+												else
+													clearTimeout(bigCycle);
+											}, 2000);
 									}
 							}
-						}
-					});
-					var marksReady = false;
-					$(forClick).each(function(i){	
-						var self = this;
-						return setTimeout(function(){
-							$(self).click();
-							var tmp = i; 
-							if(tmp == forClick.length-1)
-								return setCoast(123);
-						},1000*(i+1));
-					});
-					$(document).on('keyup','.stake.stake-input.js-focusable[name = stake]', function(e){
-						//e.stopPropagation();
-					//	$(this).keyup();
-						console.log(e);
-					});
-					$(document).on('click', '.but-place-bet', function(e){
-						//e.stopPropagation();
-					//	$(this).click();
-						console.log(e);
-					});						
-					function setCoast(coast){					
-									$('#button_accumulator').click();
-									var timer1 = setTimeout(function(){
-										$('.stake.stake-input.js-focusable[name = stake]').val(123);
-									var eve = document.createEvent('KeyboardEvent');
-									eve.initKeyboardEvent('keyup', true, true, window, false, false, false, false, 65, 65);
-									eve.keyCode = 65;
-									eve.which = 65;
-									eve.charCode = 65; 
-									//var canceled = !body.dispatchEvent(evt);
-										$('.stake.stake-input.js-focusable[name = stake]')[0].dispatchEvent(eve);
-									}, 1000);
-									var timer2 = setTimeout(function(){
-										$('.but-place-bet')[0].click();
-									}, 2000);
-					}
-					/* var checkExist = setInterval(function(){
-					   if(marksReady){
-							setTimeout(function(){
-								$('#button_accumulator').click();
-								$('[name = stake]').val(123);
-								clearInterval(checkExist);
-								},1000);
-					   }
-					}, 1000); */
-					/* $('#betslip-content').load($(this).find('#button_accumulator'), function(){
-						$(this).find('#button_accumulator').click();
-						console.log($(this).find('#button_accumulator'));
-					}); */
-					/* $(document).on('load', "#button_accumulator" , function() {
-						$(this).click();
-						}); */
-					/* $.when.apply($, ready).then(function(){
-						$('#button_accumulator').click();
-						console.log($('#button_accumulator'));
-						}); */
-					/* setTimeout(function(){
-							$("a:contains('Экспрессы')").click();
-						},(forClick.length + 1)*1000); */
 				}
 		});
 	}
